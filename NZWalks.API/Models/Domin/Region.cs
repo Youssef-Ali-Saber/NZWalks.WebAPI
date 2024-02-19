da0a32c2808a79f2b9786b99ca02a31e2a90eac2
Yousef Ali Saber <ya1654@fayoum.edu.eg>
2024-02-20 01:50:22 +0200

    Add project files.

---
+++ NZWalks.API/Models/Domin/Region.cs
@@ -0,0 +1,12 @@
+﻿namespace NZWalks.API.Models.Domin
+{
+	public class Region
+	{
+		public Guid Id { get; set; }
+		public string Name { get; set; }
+		public string Code { get; set; }
+		public string? PhotoUrl { get; set; }
+		public ICollection<Walk> Walks { get; set; }
+
+	}
+}
---
+++ NZWalks.API/Models/Domin/Walk.cs
@@ -0,0 +1,15 @@
+﻿namespace NZWalks.API.Models.Domin
+{
+	public class Walk   
+	{
+        public Guid Id { get; set; }
+        public String Name { get; set; }
+        public String Description { get; set; }
+        public double LengthInKm { get; set; }
+		public String? WalkPhotoUrl { get; set; }
+        public Guid RegionId { get; set; }
+        public Region Region { get; set; }
+        public Guid DifficultyId { get; set; }
+        public Difficulty Difficulty { get; set; } 
+}
+}
